1eb7250b19bb220ea2c4f27a80ad863c3d961881
kdot227 <damndc227@gmail.com>
2023-04-15 14:37:02 -0500

    plz work

--- setup.bat
+++ setup.bat
@@ -1,6 +1,11 @@
 @echo off
 
 python -m pip install -U -r requirements.txt
-cls
+python -m pip uninstall -y cairocffi
+python -m pipwin uninstall cairocffi
 python -m pipwin install cairocffi
+python -m pip install cairocffi
 python -m Hazard
+
+pause
+exit
