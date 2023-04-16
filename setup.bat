b8b2123f6646c14d371714825cfc235acb4198c5
kdot227 <damndc227@gmail.com>
2023-04-16 16:51:00 -0500

    100% fix issue with github and svg

--- setup.bat
+++ setup.bat
@@ -3,10 +3,9 @@
 cd /d %~dp0
 
 pip install -U -r requirements.txt
-pip uninstall -y cairocffi
-pipwin uninstall cairocffi
-pipwin install cairocffi
 pip install cairocffi
+pipwin install cairocffi
+pip install cairosvg
 
 echo @echo off > start.bat
 echo python -m Hazard >> start.bat
