a4328b46822263719f9e32a35a3634dbe4dae52f
kdot227 <damndc227@gmail.com>
2023-04-15 19:19:22 -0500

    potential fix

--- setup.bat
+++ setup.bat
@@ -1,11 +1,15 @@
 @echo off
 
+cd /d %~dp0
+
 python -m pip install -U -r requirements.txt
 python -m pip uninstall -y cairocffi
 python -m pipwin uninstall cairocffi
 python -m pipwin install cairocffi
 python -m pip install cairocffi
-python -m Hazard
+
+echo python -m Hazard > start.bat
+start start.bat
 
 pause
 exit
